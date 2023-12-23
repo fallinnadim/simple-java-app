3d0f2b362c93f0d3e5a62e531f2e55cb800e47f9
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-23 15:13:53 +0700

    test1234

--- Jenkinsfile
+++ Jenkinsfile
@@ -43,10 +43,5 @@ pipeline {
                 }
             }
         }
-        stage('Deliver') {
-            steps {
-                sh './jenkins/scripts/deliver.sh'
-            }
-        }
     }
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -18,6 +18,6 @@ public class AppTest {
     @Test
     public void testAppMessage() {
         App app = new App();
-        assertEquals("Hello Imelia!", app.getMessage());
+        assertEquals("Hello Worldzzz!", app.getMessage());
     }
 }
