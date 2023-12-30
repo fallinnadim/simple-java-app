67a6fb8e82c48dd02d657147c305c6d18dbf7a66
fallinnadim <nadim@mhs.mdp.ac.id>
2023-12-30 17:19:31 +0700

    Test Git Webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Ada sesuatu tentang matamu";
+    private static final String MESSAGE = "selalu senang melihat matamu";
 
     public App() {
     }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -18,6 +18,6 @@ public class AppTest {
     @Test
     public void testAppMessage() {
         App app = new App();
-        assertEquals("Ada sesuatu tentang matamu", app.getMessage());
+        assertEquals("selalu senang melihat matamu", app.getMessage());
     }
 }
